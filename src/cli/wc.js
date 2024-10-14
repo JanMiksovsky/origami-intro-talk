12f63b18642a08b67f8d1b1e59a469b4015e7221
Jan Miksovsky <jan@miksovsky.com>
2024-10-14 15:41:00 -0700

    Examples

--- src/cli/wc.js
+++ src/cli/wc.js
@@ -1,4 +1,7 @@
 export default function wordCount(input) {
+  if (!input) {
+    return 0;
+  }
   const text =
     input instanceof Uint8Array ? new TextDecoder().decode(input) : input;
   const words = text.split(/\s+/).filter((word) => word);
